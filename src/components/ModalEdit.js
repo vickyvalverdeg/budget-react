cebf0df32ea9470ceee21ba0be0405446575142f
Vicky Valverde <vickyvalverdeg@gmail.com>
2021-07-25 23:00:56 -0500

    render entryForm in modal

--- src/components/ModalEdit.js
+++ src/components/ModalEdit.js
@@ -1,19 +1,20 @@
-import React from 'react'
-import { Button, Modal } from "semantic-ui-react"
+import React from "react";
+import { Button, Modal } from "semantic-ui-react";
+import NewEntryForm from "./NewEntryForm";
 
-function ModalEdit({isOpen, setIsOpen}) {
-    return (
-        <Modal open={isOpen}>
-            <Modal.Header>Edit Entry</Modal.Header>
-            <Modal.Content>
-                <Modal.Description>Something here</Modal.Description>
-            </Modal.Content>
-            <Modal.Actions>
-                <Button onClick={()=> setIsOpen(false)}>Close</Button>
-                <Button>Save</Button>
-            </Modal.Actions>
-        </Modal>
-    )
+function ModalEdit({ isOpen, setIsOpen }) {
+  return (
+    <Modal open={isOpen}>
+      <Modal.Header>Edit Entry</Modal.Header>
+      <Modal.Content>
+        <NewEntryForm />
+      </Modal.Content>
+      <Modal.Actions>
+        <Button onClick={() => setIsOpen(false)}>Close</Button>
+        <Button>Save</Button>
+      </Modal.Actions>
+    </Modal>
+  );
 }
 
-export default ModalEdit
+export default ModalEdit;
