4bd68e0960e9bee6c84621677dfcdda49f69df51
Vicky Valverde <vicky.valverde@telus.com>
2022-04-02 23:10:51 -0500

    feat: fix warnings

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ function App() {
       setEntries(newEntries)
       resetEntry()
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isOpen])
 
   useEffect(()=> {
--- src/components/EntryForm.js
+++ src/components/EntryForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Checkbox, Form, Segment } from "semantic-ui-react";
 
 function EntryForm({
